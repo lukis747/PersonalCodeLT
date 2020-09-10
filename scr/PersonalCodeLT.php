39bf6d4756c57361129f976375a5a37962cb6df5
laluan <lgaidziunas@gmail.com>
2020-09-10 18:28:31 +0300

    initial commit

---
+++ scr/PersonalCodeLT.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Lukjon\PersonalCodeLT;
+
+
+class PersonalCodeLT
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
